e9d5da2098fa3ae1060756107a2bd22051e9d477
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-07-14 12:19:36 +0200

    Added booleans

---
+++ src/Booleans.java
@@ -0,0 +1,16 @@
+// Java booleans
+
+public class Booleans {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFemale = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFemale);
+
+        // Boolean expressions
+        int x = 5;
+        int y = 7;
+        boolean isGreater = x > y;
+        System.out.println(isGreater);
+    }
+}
