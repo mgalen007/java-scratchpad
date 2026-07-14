1c505835d4d48a6470b3477d84c5b91905634baa
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-07-14 12:02:05 +0200

    Added data types

---
+++ src/DataTypes.java
@@ -0,0 +1,23 @@
+// Data types in Java
+
+/*
+1. Primitive (lowercase): e.g. int, float, char, boolean
+2. Non-primitive / reference (uppercase): e.g. String, classes, arrays
+*/
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Primitives
+
+        byte num1 = 1;
+        short num2 = 130;
+        int num3 = 1_000_000;
+        long num4 = 1_000_000_000;
+        char gender = 'M';
+        boolean isMale = true;
+
+        // Non-primitives
+        String name = "Galen";
+        int[] numbers = {1, 2, 3, 4, 5};
+    }
+}
