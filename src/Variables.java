d5f284edc185ec2a5af8ba9c954721775f082625
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-07-14 12:01:50 +0200

    Added variables file

---
+++ src/Variables.java
@@ -0,0 +1,21 @@
+// Java variables
+
+public class Variables {
+    public static void main(String[] args) {
+        // Syntax: dataType name = value;
+
+        String name = "Galen";
+        int age = 17;
+        float height = 1.79f;
+        double score = 92.23;
+        char gender = 'M';
+        boolean isSingle = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Score: " + score);
+        System.out.println("Gender: " + gender);
+        System.out.println("Single: " + isSingle);
+    }
+}
