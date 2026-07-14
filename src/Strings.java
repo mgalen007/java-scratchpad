c1490bb1ac83dd5951b8e39bf66d57ecfd954ac3
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-07-14 12:13:49 +0200

    Added strings

---
+++ src/Strings.java
@@ -0,0 +1,14 @@
+// Java strings
+
+public class Strings {
+    public static void main(String[] args) {
+        String greeting = "Hello";
+        String spaces = " h e l l o ";
+
+        // some string methods (strings are objects, so they have methods)
+        System.out.println("Length: " + greeting.length());
+        System.out.println("Uppercase : " + greeting.toUpperCase());
+        System.out.println("Lowercase: " + greeting.toLowerCase());
+        System.out.println("Trimmed: " + spaces.trim());
+    }
+}
